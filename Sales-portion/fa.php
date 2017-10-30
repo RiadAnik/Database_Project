1cd42ecdd17c3f86572c5036c2d1b2d8a047e2e6
Riad Mahmud Anik <riadanik009@gmail.com>
2017-10-30 20:40:44 +0600

    Add files via upload

---
+++ Sales-portion/fa.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html> <!-- The new doctype -->
+
+<html>
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<title>IT_MART</title>
+
+	<style>
+        body{
+            background-color:#667A44 ;
+        }
+
+    header,footer,
+article,section,
+hgroup,nav,
+figure{
+	/* Giving a display value to the HTML5 rendered elements: */
+	display:block;
+}
+
+article .line{
+	/* The dividing line inside of tarticle is darker: */
+	background-color:#15242a;
+	border-bottom-color:#204656;
+	margin:1.3em 0;
+}
+
+footer .line{
+	margin:2em 0;
+}
+
+nav{
+	background:url(img/gradient_light.jpg) repeat-x 50% 50% #f8f8f8;
+	padding:0 5px;
+	position:absolute;
+	right:0;
+	top:4em;
+
+	border:1px solid #FCFCFC;
+
+	-moz-box-shadow:0 1px 1px #333333;
+	-webkit-box-shadow:0 1px 1px #333333;
+	box-shadow:0 1px 1px #333333;
+}
+
+nav ul li{
+	display:inline;
+}
+
+nav ul li a,
+nav ul li a:visited{
+	color:#565656;
+	display:block;
+	float:left;
+	font-size:1.25em;
+	font-weight:bold;
+	margin:5px 2px;
+	padding:7px 10px 4px;
+	text-shadow:0 1px 1px white;
+	text-transform:uppercase;
+}
+
+nav ul li a:hover{
+	text-decoration:none;
+	background-color:#f0f0f0;
+}
+
+nav, article, nav ul li a,figure{
+	/* Applying CSS3 rounded corners: */
+	-moz-border-radius:10px;
+	-webkit-border-radius:10px;
+	border-radius:10px;
+}
+
+
+
+
+/* Article styles: */
+
+#page{
+	width:960px;
+	margin:0 auto;
+	position:relative;
+}
+
+article{
+	background-color:#213E4A;
+	margin:3em 0;
+	padding:20px;
+
+	
+}
+
+figure{
+	border:3px solid #142830;
+	float:right;
+	height:300px;
+	margin-left:15px;
+	overflow:hidden;
+	width:500px;
+}
+
+figure:hover{
+	-moz-box-shadow:0 0 2px #4D7788;
+	-webkit-box-shadow:0 0 2px #4D7788;
+	box-shadow:0 0 2px #4D7788;
+}
+
+figure img{
+	margin-left:-60px;
+}
+
+/* Footer styling: */
+
+footer{
+	margin-bottom:30px;
+	text-align:center;
+	font-size:0.825em;
+}
+
+footer p{
+	margin-bottom:-2.5em;
+	position:relative;
+}
+
+footer a,footer a:visited{
+	color:#cccccc;
+	background-color:#213e4a;
+	display:block;
+	padding:2px 4px;
+	z-index:100;
+	position:relative;
+}
+
+footer a:hover{
+	text-decoration:none;
+	background-color:#142830;
+}
+
+footer a.by{
+	float:left;
+
+}
+
+footer a.up{
+	float:right;
+}
+        
+     a.ex2:hover, a.ex2:active {font-size: 150%;}
+    </style>
+
+</head>
+<body>
+
+	<section id="page"> <!-- Defining the #page section with the section tag -->
+
+	<header> <!-- Defining the header section of the page with the appropriate tag -->
+
+		<img src="logo.PNG"/>
+
+        <p font:"italian">Next Generation Technology</p>
+        <nav class="clear"> <!-- The nav link semantically marks your main site navigation -->
+
+			<ul>
+
+				<li><a class="ex2" href="index.php">Home</a></li>
+				<li><a class="ex2" href="loginform.inc.php">Login</a></li>
+
+			</ul>
+        </header>
+	
+        
+        
+        <!-- Article 1 start -->
+
+	<div class="line"></div>  <!-- Dividing line -->
+
+	<article id="article1"> <!-- The new article tag. The id is supplied so it can be scrolled into view. -->
+
+		
+			</figure>
+        
+        <marquee bgcolor="red">Address:XYZ Center,shop no:35-37,3rd floor.Contact:010121212,011212121212,02-12345 </marquee>
+    
+
+			<h1>Company Profile</h1>
+    
+<h1>IT_MART</h1>
+    
+<h2>Built in Quality & Reliability</h2>
+    
+<h4>Overview </h4> 
+<p>Computer Source Limited (CSL) is the largest IT Distributor in Bangladesh. For CSL, its customers are not only the most important stakeholders; rather they are regarded as the most valued partners as well. CSL is pledge bound to ensure the best services and solutions to its customers, offering a rewarding experience in purchasing and owning a CSL product. Customers' satisfaction and loyalty oblige CSL for furthering efforts and deliver the largest array of IT products through our 45 offices nationwide. 
+
+Since 1993, Computer Source has connected technology solution providers with users nationwide, identifying markets and technologies that shape the IT industry. Today, Computer Source remains at the forefront of the technology marketplace, bringing the latest products and services to market and finding new ways to bring value to its customers. 
+</p>
+<h4>Vision </h4>
+<p>
+"Maximize human resource capability through technological advancement for a sustainable development of the country." 
+</p>
+<h4>Mission </h4>
+<p>Establish Computer Source as the leading company and reach its products and services to every nook and corner of the country. </p>
+<h4>Objective </h4>
+<p>
+To ensure and sustain win-win situation for all the stakeholders of the company; while offering value for money in terms of product quality and service. 
+</p>
+<h4>History of Computer Source </h4>
+<p>
+Computer Source was founded in 1993 by two university graduates. Since the company's inception, CSL business executives and engineers have been leaders in the market in selling, distributing and providing solution for technology products. Today, with more than 1200 employees nationwide, this tradition of marketing innovation continues with industry-leading products and solutions in the company's core business areas. In 2000 the company became a limited company by shares, and currently the company has seven directors.     
+</p>
+<h4>Major Products and Product Line </h4>
+<p>
+The company offers a broad array of solutions and services to thousands by distributing and marketing IT products from nearly 47 world renowned brands. Apple, Antec, Double A, AEG, Apacer, AverMedia, Buy48, Cheval, Corsair, Cisco, CSM, Cyberoam, Dell, EMC, Emerson, Fujitsu, HP, Intel, Konica Minolta, Kodak, Lexmark, Linksys, Logitech, Maxtor, Microlab, Microsoft, MSI, Molex, NetApp, Norton, Oracle, Perfect, Philips, Prolink, Polycom, Razer, Rosenberger, Ruckus, Samsung, Sony, Steelseries, Symantec, Symantec, Targus, Transcend, Vmware, Western Digital (WD) are the stars. Service support is the key differentiation factor for the company over its competitors. The company has created its own warranty brand called 'BUY 48' under which it offers solution to its valued customers within 48 hours of warranty claim. We have established a State-of-the-art 32,000sqft. Central Customer Care centre operated by 210+ Service Engineers to satisfy the needs of our customers. 
+</p>    
+
+		</div>
+
+	</article>
+
+	<!-- Article 1 end -->
+        
+        
+        <footer> <!-- Marking the footer section -->
+
+			<div class="line"></div>
+
+			<p>Copyright 2010 - YourSite.com</p> <!-- Change the copyright notice -->
+        </footer>
+
+	</body>
+
+</html>
